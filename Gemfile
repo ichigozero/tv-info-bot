e62694a770b01d28fa53216394d5433084cf208f
Gary Sentosa <gary.sentosa@gmail.com>
2021-02-05 21:32:11 +0900

    Use let for object instance

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
-ruby '2.7.0'
+ruby '3.0.0'
+
+gem 'rspec'
--- spec/tv_info_spec.rb
+++ spec/tv_info_spec.rb
@@ -2,17 +2,17 @@ require 'rss'
 require 'tv_info'
 
 describe TvInfo do
-  before(:each) { @tv_info = TvInfo.new }
+  let(:tv_info) { TvInfo.new }
 
   describe '.get_program_summaries_of_multiple_actors' do
     it 'returns program summaries given actor names' do
       rss = RSS::Parser.parse(File.read('spec/fixtures/rss.xml'), false)
       allow(RSS::Parser).to receive(:parse).and_return(rss)
 
-      summaries = @tv_info.get_program_summaries_of_multiple_actors(
+      summaries = tv_info.get_program_summaries_of_multiple_actors(
         %w[有村架純 よしのちとせ]
       )
-      expect(summaries.values.flatten.length).to eq(88)
+      expect(summaries.length).to eq(88)
     end
   end
 
@@ -21,13 +21,13 @@ describe TvInfo do
       rss = RSS::Parser.parse(File.read('spec/fixtures/rss.xml'), false)
       allow(RSS::Parser).to receive(:parse).and_return(rss)
 
-      summaries = @tv_info.get_program_summaries('有村架純')
+      summaries = tv_info.get_program_summaries('有村架純')
       expect(summaries.length).to eq(44)
     end
   end
 
   describe '.compose_url' do
-    subject { @tv_info.send(:compose_url, '有村架純') }
+    subject { tv_info.send(:compose_url, '有村架純') }
 
     it 'returns URL string containing actor name' do
       expected_url = 'https://www.tvkingdom.jp/rss/'\
@@ -42,12 +42,13 @@ describe TvInfo do
     rss = RSS::Parser.parse(File.read('spec/fixtures/rss.xml'), false)
 
     subject do
-      @tv_info.send(:get_program_summary, rss.item)
+      tv_info.send(:get_program_summary, '有村架純', rss.item)
     end
 
     it 'returns program summary' do
       expect(subject).to eq(
         {
+          actor_name: '有村架純',
           title: '映画「花束みたいな恋をした」スペシャル',
           channel: 'ＴＢＳチャンネル１ 最新ドラマ・音楽・映画(Ch.616)',
           schedule: {
@@ -61,7 +62,7 @@ describe TvInfo do
 
   describe '.extract_program_title' do
     raw_title = '[映]かぞくいろ　-RAILWAYS　わたしたちの出発-[SS][字]'
-    subject { @tv_info.send(:extract_program_title, raw_title) }
+    subject { tv_info.send(:extract_program_title, raw_title) }
 
     it 'returns program title only' do
       expect(subject).to eq('かぞくいろ　-RAILWAYS　わたしたちの出発-')
@@ -70,7 +71,7 @@ describe TvInfo do
 
   describe '.extract_channel_name' do
     raw_description = '1/28 14:40～16:50 [ＷＯＷＯＷシネマ]'
-    subject { @tv_info.send(:extract_channel_name, raw_description) }
+    subject { tv_info.send(:extract_channel_name, raw_description) }
 
     it 'returns channel name' do
       expect(subject).to eq('ＷＯＷＯＷシネマ')
@@ -81,7 +82,7 @@ describe TvInfo do
     raw_date = '2021-01-28T14:40+09:00'
     raw_description = '1/28 14:40～16:50 [ＷＯＷＯＷシネマ]'
     subject do
-      @tv_info.send(:extract_program_schedule, raw_date, raw_description)
+      tv_info.send(:extract_program_schedule, raw_date, raw_description)
     end
 
     it 'returns program schedule' do
